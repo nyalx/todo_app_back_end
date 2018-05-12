0a7580b7e41099744f51d9d1ffeebc1ed1663dbc
nya evans <nyae0703@gmail.com>
2018-05-12 19:21:37 +0000

    Added links

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,27 @@
 class TodoController < ApplicationController
     def index
-        
+        id= 2
+        if id== "1"
+            @todo_description = "do homework"
+            @todo_pomodoro_estimate = 7
+        end
+       id= 3
+       if id== "2"
+           @todo_description = "take out trash"
+           @todo_promodo_estimate = 8
+       end
+    end
+    def show
+        todo_id =params[:id]
+        if todo_id== "1"
+           @todo_description = "do homework"
+           @todo_promodo_estimate = 8
+        elsif todo_id == "2"
+           @todo_description = "take out trash"
+           @todo_promodo_estimate = 8
+        elsif todo_id =="3"
+           @todo_description = "take out trash"
+           @todo_promodo_estimate = 8
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get '/todo/index' => 'todo#index'
+get '/todo/index' => 'todo#index'
+get 'todo/show/:id' => 'todo#show'
 end
