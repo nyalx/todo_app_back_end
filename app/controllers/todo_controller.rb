dd909b4bd5ddc1ab8a444e496f05c767a004e22c
nya evans <nyae0703@gmail.com>
2018-05-13 15:46:18 +0000

    added new files

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,18 +1,9 @@
 class TodoController < ApplicationController
     def index
-        id= 2
-        if id== "1"
-            @todo_description = "do homework"
-            @todo_pomodoro_estimate = 7
-        end
-       id= 3
-       if id== "2"
-           @todo_description = "take out trash"
-           @todo_promodo_estimate = 8
-       end
     end
     def show
-        todo_id =params[:id]
+        #Get the number the user typed in the URL
+        todo_id =params[:id] 
         if todo_id== "1"
            @todo_description = "do homework"
            @todo_promodo_estimate = 8
@@ -21,7 +12,10 @@ class TodoController < ApplicationController
            @todo_promodo_estimate = 8
         elsif todo_id =="3"
            @todo_description = "Do it by 8:00 PM"
-           @todo_promodo_estimate = 8
+           @todo_promodo_estimate = 4
+         elsif todo_id=="4"
+         @todo_description = "study for global and chemistry"
+         @todo_promodo_estimate = 8
         end
     end
 end
